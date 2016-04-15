87d2692095e78302299a6fe5fdcccd7ca289b8be
Giovani Ferreira <silva.giovanif@gmail.com>
2016-04-15 08:43:15 -0300

    Improviments in modular parallel implementation

--- modular_serial/main.c
+++ modular_serial/main.c
@@ -11,7 +11,7 @@
 #include"generator.h"
 
 #define MAX 50000               /* How many hashes will be generated */
-#define DESIRED_COLLISION 17    /* How many bytes must be equal to be considered a collision */
+#define DESIRED_COLLISION 15    /* How many bytes must be equal to be considered a collision */
 #define BUFFER_SIZE 22          /* The size for create a string representation of a number */
 
 #define WARNS_AFTER 10000       /* Display a warning of status after X repetitions */
@@ -43,6 +43,7 @@ int main(int argc, char *argv[])
     printf("==> Searching for collisions in hashes\n");
 
     for (int i = 0; i < MAX; i++){
+        printf("Index: %d\n", i);
         for (int j = i + 1; j < MAX; j++){
             byte_collisions = 0;
 
